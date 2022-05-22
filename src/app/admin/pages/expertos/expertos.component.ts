b3c3ec38ca8f5f5a3bc17aecd39869a7e03e00a8
Jonnathan Uzhca <jonathan.uzhca0106@gmail.com>
2022-05-22 09:26:36 -0500

    creacion del  experto y simuladores completo

--- src/app/admin/pages/expertos/expertos.component.ts
+++ src/app/admin/pages/expertos/expertos.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Customer, Representative } from './domain/customer';
 import { CustomerService } from './domain/customerservice';
-import { Product } from './domain/product';
 import { ProductService } from './domain/productservice';
 import { Table } from 'primeng/table';
 
@@ -21,25 +20,14 @@ import { Table } from 'primeng/table';
 
 })
 export class ExpertosComponent implements OnInit {
-
+  customers1: Customer[];
+  selectedCustomer1: Customer;
 
   customers2: Customer[];
+  selectedCustomer2: Customer;
 
   customers3: Customer[];
-
-  selectedCustomers1: Customer[];
-
-  selectedCustomer: Customer;
-
-  representatives: Representative[];
-
-  statuses: any[];
-
-  products: Product[];
-
-  rowGroupMetadata: any;
-
-  activityValues: number[] = [0, 100];
+  selectedCustomer3: Customer;
 
   @ViewChild('dt') table: Table;
   private customerService: CustomerService; private productService: ProductService;
@@ -50,59 +38,8 @@ export class ExpertosComponent implements OnInit {
 
   ngOnInit(): void {
    
-    this.customerService.getCustomersMedium().then(customers => this.customers2 = customers);
    
-    this.productService.getProductsWithOrdersSmall().then(data => this.products = data);
-
-    this.representatives = [
-      { name: 'Amy Elsner', image: 'amyelsner.png' },
-      { name: 'Anna Fali', image: 'annafali.png' },
-      { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
-      { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
-      { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
-      { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
-      { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
-      { name: 'Onyama Limba', image: 'onyamalimba.png' },
-      { name: 'Stephen Shaw', image: 'stephenshaw.png' },
-      { name: 'XuXue Feng', image: 'xuxuefeng.png' }
-    ];
-
-    this.statuses = [
-      { label: 'Unqualified', value: 'unqualified' },
-      { label: 'Qualified', value: 'qualified' },
-      { label: 'New', value: 'new' },
-      { label: 'Negotiation', value: 'negotiation' },
-      { label: 'Renewal', value: 'renewal' },
-      { label: 'Proposal', value: 'proposal' }
-    ];
   }
 
-  onSort() {
-    this.updateRowGroupMetaData();
-  }
-
-  updateRowGroupMetaData() {
-    this.rowGroupMetadata = {};
-
-    if (this.customers3) {
-      for (let i = 0; i < this.customers3.length; i++) {
-        const rowData = this.customers3[i];
-        const representativeName = rowData.representative.name;
-
-        if (i === 0) {
-          this.rowGroupMetadata[representativeName] = { index: 0, size: 1 };
-        }
-        else {
-          const previousRowData = this.customers3[i - 1];
-          const previousRowGroup = previousRowData.representative.name;
-          if (representativeName === previousRowGroup) {
-            this.rowGroupMetadata[representativeName].size++;
-          }
-          else {
-            this.rowGroupMetadata[representativeName] = { index: i, size: 1 };
-          }
-        }
-      }
-    }
-  }
+ 
 }
