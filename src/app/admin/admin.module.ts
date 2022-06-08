6938773cf5a98a3764c93b70e483f520292297a0
Claudio Mldo <claum0395@gmail.com>
2022-06-08 00:00:23 -0500

    Fixed error

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,30 +1,35 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AdminRoutingModule } from './admin-routing.module';
-import { AdminComponent } from './admin.component';
-import { SharedModule } from '../shared/shared.module';
-import { ComponentsModule } from './components/components.module';
-import { HomeComponent } from './pages/home/home.component';
-import { ExpertosComponent } from './pages/expertos/expertos.component';
-import { SimuladoresComponent } from './pages/simuladores/simuladores.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { AdminRoutingModule } from "./admin-routing.module";
+import { AdminComponent } from "./admin.component";
+import { SharedModule } from "../shared/shared.module";
+import { ComponentsModule } from "./components/components.module";
+import { HomeComponent } from "./pages/home/home.component";
+import { ExpertosComponent } from "./pages/expertos/expertos.component";
+import { SimuladoresComponent } from "./pages/simuladores/simuladores.component";
 
-import { DatosAdminComponent } from './pages/datos-admin/datos-admin.component'
-import { NuevoEjercitarioComponent } from './pages/ejercitario/nuevo-ejercitario.component';
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
-import { DropdownModule } from 'primeng/dropdown';
-import { FileUploadModule } from 'primeng/fileupload';
-import { NgxDropzoneModule } from 'ngx-dropzone';
+import { DatosAdminComponent } from "./pages/datos-admin/datos-admin.component";
+import { NuevoEjercitarioComponent } from "./pages/ejercitario/nuevo-ejercitario.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { DropdownModule } from "primeng/dropdown";
+import { FileUploadModule } from "primeng/fileupload";
+import { NgxDropzoneModule } from "ngx-dropzone";
 
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { UsuariosComponent } from './pages/usuarios/usuarios.component';
+import { InputTextareaModule } from "primeng/inputtextarea";
+import { UsuariosComponent } from "./pages/usuarios/usuarios.component";
 
-import { RouterModule } from '@angular/router';
-import { TopBarPageExpertoComponent } from '../expert/pages/StructurePage-Experto/top-bar-page-experto/top-bar-page-experto.component';
-
-
+import { RouterModule } from "@angular/router";
 
 @NgModule({
-  declarations: [AdminComponent, HomeComponent, ExpertosComponent, SimuladoresComponent, NuevoEjercitarioComponent, DatosAdminComponent, UsuariosComponent] ,
+  declarations: [
+    AdminComponent,
+    HomeComponent,
+    ExpertosComponent,
+    SimuladoresComponent,
+    NuevoEjercitarioComponent,
+    DatosAdminComponent,
+    UsuariosComponent,
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule,
@@ -36,7 +41,6 @@ import { TopBarPageExpertoComponent } from '../expert/pages/StructurePage-Expert
     FileUploadModule,
     NgxDropzoneModule,
     InputTextareaModule,
-
-  ]
+  ],
 })
-export class AdminModule { }
+export class AdminModule {}
